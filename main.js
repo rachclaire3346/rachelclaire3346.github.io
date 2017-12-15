ad010fd1e4c1ee80c6f8670995a301070625f040
Rachel Carter <31392063+rachclaire3346@users.noreply.github.com>
2017-12-15 11:48:22 -0500

    new portfolio

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+	$('.carousel').carousel();
+});
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip({delay: 50});
+});
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
